ba1a81777ab6bb187e7b3ebd793512bf85d281e3
soxft <email@xcsoft.top>
2024-02-20 21:11:49 +0800

    Fix: 优化分层

--- src/Ext.php
+++ src/Ext.php
@@ -16,11 +16,25 @@ abstract class Ext
     protected function key(string $identifier): string
     {
         if ($this->keyPrefix === '') {
-            if (str_starts_with($identifier, ':')) {
-                $this->keyPrefix = 'ratelimit';
-            } else {
-                $this->keyPrefix = ':ratelimit';
-            }
+            $this->keyPrefix = 'ratelimit';
+        } else {
+            $this->keyPrefix = "ratelimit:{$this->keyPrefix}";
+        }
+
+
+        // 如何 存在 : 结尾 则删除 :
+        if (str_ends_with($this->keyPrefix, ':')) {
+            $this->keyPrefix = substr($this->keyPrefix, 0, -1);
+        }
+
+        // 用户传入的 identifier 以 : 开头 则删除 :
+        if (str_starts_with($identifier, ':')) {
+            $identifier = substr($identifier, 1);
+        }
+
+        // 用户传入的 identifier 以 : 结尾 则删除 :
+        if (str_ends_with($identifier, ':')) {
+            $identifier = substr($identifier, 0, -1);
         }
 
         return "{$this->keyPrefix}:{$identifier}:{$this->rate->getInterval()}";
